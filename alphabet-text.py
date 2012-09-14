2948753f99aa8b0ab35ccad4114d5aab13f0bde5
wsmithril <wsmithril@gmail.com>
2012-09-14 15:01:34 +0000

    Show basic alphabetics

---
+++ alphabet-text.py
@@ -0,0 +1,14 @@
+import rpi5110 as lcd
+
+def main():
+    lcd.init()
+    lcd.text(map(chr, range(0x61, 0x7B)))
+    lcd.locate(2, 0)
+    lcd.text(map(chr, range(0x41, 0x5B)))
+    lcd.locate(4, 0)
+
+    lcd.backlight(1)
+
+if __name__ == "__main__":
+    main()
+

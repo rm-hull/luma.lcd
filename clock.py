d9be1fd0efae6f0bd606851d3889395b4b31e25e
Richard Hull <rm_hull@yahoo.co.uk>
2013-01-19 21:49:55 +0000

    More examples

--- clock.py
+++ clock.py
@@ -6,14 +6,15 @@ import time
 def main():
     lcd.init()
     lcd.text("Hello world!")
+    lcd.locate(4, 0)
+    lcd.text("Charlotte :-)")
     while True:
         lcd.locate(1, 0)
         lcd.text(time.strftime("%d %b %Y", time.localtime()))
         lcd.locate(2, 0)
         lcd.text(time.strftime("%H:%M:%S", time.localtime()))
-        lcd.locate(4, 0)
-        lcd.text("Charlotte :-)")
-        time.sleep(1)
+
+        time.sleep(0.25)
         
 if __name__ == "__main__":
     main()
---
+++ smiley.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import pcd8544 as lcd
+
+smiley = [ 0x3C,0x6E,0xD3,0xD3,0xDF,0xD3,0xD3,0x6E,0x3C ]
+
+ch  = [0x00,0x7E,0x42,0x7E,0x4A,0x72,0x42,0x00]
+
+def main():
+    lcd.init()
+    lcd.cls()
+    lcd.data(smiley)
+    lcd.locate(2,2)
+    lcd.data(ch)
+    lcd.locate(0,3)
+    lcd.text("c.a.h.")
+    lcd.backlight(1)
+
+if __name__ == "__main__":
+    main()

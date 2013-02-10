1cfd3b38323ab519e934975888b4be9c69a51c8b
Richard Hull <rm_hull@yahoo.co.uk>
2013-02-10 00:18:49 +0000

    First version of working data plotter

--- examples/plotter.py
+++ examples/plotter.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
+# TODO
+#  1. Incorporate some kind of rolling average
+#  2. Smaller fonts
+#  3. X/Y scale
+#
+
 import os
 import sys
 
 if os.name != 'posix':
     sys.exit('platform not supported')
 
-from datetime import datetime
+import time
 import math
 import psutil
-import pcd8544.lcd as lcd
+from pcd8544 import lcd
+from PIL import Image,ImageDraw
 
 class RingBuffer:
     def __init__(self, size):
@@ -19,13 +26,16 @@ class RingBuffer:
         self.data.pop(0)
         self.data.append(x)
 
-    def get(self):
-        return self.data
+    def __getitem__(self, i):
+        return self.data[i]
+
+    def __iter__(self):
+        return iter(self.data)
 
 class LooseLabels:
     """ Implements loose axis labelling from "Graphics Gems",
         /Nice Numbers for Graph Labels/, p. 61-63."""
-    def __init__(self, low, high, ntick):
+    def __init__(self, (low, high), ntick):
         self.range = []
         r = self.__nice_num(high - low, False)
         d = self.__nice_num(r / (ntick - 1), True)
@@ -39,16 +49,22 @@ class LooseLabels:
             self.range.append(round(i, nfrac))
             i += d
 
+    def __getitem__(self, i):
+        return self.range[i]
+
     def __nice_num(self, x, rounding=False):
+        if x == 0:
+            return 1
+
         exp = int(math.floor(math.log10(x)))
-        frac = x / math.pow(10, exp)
+        frac = x / 10**exp
 
         if rounding:
             nf = self.__scale(frac, [(1.5,1), (3,2), (7,5)], lambda x,y: x < y)
         else:
             nf = self.__scale(frac, [(1,1), (2,2), (5,5)], lambda x,y: x <= y)
 
-        return nf * math.pow(10, exp)
+        return nf * 10**exp
 
     def __scale(self, x, lookups, fn, default=10):
         for y in lookups:
@@ -57,27 +73,79 @@ class LooseLabels:
 
         return default
 
+class Scale:
+    def __init__(self, (low,high), size):
+        """ Creates a function which will translate any min/max tuple
+            to the correct scaled value for display on the LCD panel """
+        self.labels = LooseLabels((low,high), 5)
+        offset = -self.labels[0]
+        diff = (self.labels[-1] - self.labels[0]) / size
+        self.fn = lambda y: size - int((y + offset) / diff)
+
+    def __call__(self,i):
+        return self.fn(i)
+
 class LoadAverage:
     def name(self):
         return "Load Average"
+
+    def format(self, data):
+        return "%.2f %.2f %.2f" % data
 
     def data(self):
         return os.getloadavg()
 
 class CpuTemperature:
     def name(self):
-        return "CPU Temp 'C"
+        return "CPU Temp"
+
+    def format(self, data):
+        return "%.1f'C" % data
 
     def data(self):
         res = os.popen("vcgencmd measure_temp").readline()
         return tuple([float(res.replace("temp=","").replace("'C\n",""))])
+
+def min_max(buf):
+    """ Gets the smallest and largest value from the tuples in the ring buffer """
+    data = filter(lambda x: x <> None, buf)
+    smallest = min(map(min, data))
+    largest = max(map(max, data))
+    return (smallest, largest)
+
+def rolling_avg(cumulative_lookup, start, stop, step_size, window_size):
+    for t in range(start + window_size, stop, step_size):
+        total = cumulative_lookup[t] - cumulative_lookup[t - window_size]
+        yield total / window_size
+
+def render(buf):
+    """ Returns an image rendering of the data in the buffer """
+    im = Image.new('1', (84,48))
+    draw = ImageDraw.Draw(im)
+    scale = Scale(min_max(buf), im.size[1])
+
+    for x,data in enumerate(buf):
+        if data <> None:
+            for y in data:
+                draw.point((x,scale(y)), 1)
+
+    del draw
+    return im
 
 def main():
     lcd.init()
     lcd.backlight(1)
     sample = CpuTemperature()
     #sample = LoadAverage()
-    lcd.text("%.2f" % sample.data())
+    buf = RingBuffer(84)
+
+    while True:
+        current = sample.data()
+        buf.append(current)
+        lcd.image(render(buf))
+        lcd.locate(0,0)
+        lcd.text(sample.format(current))
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()

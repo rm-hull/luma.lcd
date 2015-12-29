de0df0c6e0f41dc05a8cf2eecb5dd38d03eb7fe0
Richard Hull <rm_hull@yahoo.co.uk>
2015-12-29 23:37:43 +0000

    Updated for latest psutil (patch from @DougieLawson) - fixes #9

--- examples/sys_info.py
+++ examples/sys_info.py
@@ -33,30 +33,30 @@ def bytes2human(n):
             value = int(float(n) / prefix[s])
             return '%s%s' % (value, s)
     return "%sB" % n
-    
+
 def cpu_usage():
     # load average, uptime
-    uptime = datetime.now() - datetime.fromtimestamp(psutil.BOOT_TIME)
+    uptime = datetime.now() - datetime.fromtimestamp(psutil.boot_time())
     av1, av2, av3 = os.getloadavg()
     return "Load:%.1f %.1f %.1f Up: %s" \
             % (av1, av2, av3, str(uptime).split('.')[0])
-    
-def mem_usage():
-    usage = psutil.phymem_usage()
-    return "Mem: %s %.0f%%" \
-            % (bytes2human(usage.used), 100 - usage.percent)  
 
-    
+def mem_usage():
+    usage = psutil.virtual_memory()
+    return "Mem: %s %.0f%%" \
+            % (bytes2human(usage.used), 100 - usage.percent)
+
+
 def disk_usage(dir):
     usage = psutil.disk_usage(dir)
     return "SD:  %s %.0f%%" \
-            % (bytes2human(usage.used), usage.percent)  
+            % (bytes2human(usage.used), usage.percent)
 
 def network(iface):
-    stat = psutil.network_io_counters(pernic=True)[iface]
+    stat = psutil.net_io_counters(pernic=True)[iface]
     return "%s:Tx%s,Rx%s" % \
            (iface, bytes2human(stat.bytes_sent), bytes2human(stat.bytes_recv))
-    
+
 def stats():
     lcd.cls()
     lcd.locate(0,0)
@@ -66,13 +66,13 @@ def stats():
     lcd.locate(0,2)
     lcd.text(disk_usage('/'))
     lcd.locate(0,3)
-    lcd.text(network('wlan0'))
+    lcd.text(network('eth0'))
     lcd.locate(0,4)
-        
+
 def main():
     lcd.init()
     lcd.backlight(1)
     stats()
-    
+
 if __name__ == "__main__":
     main()

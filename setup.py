972b81d7ef51dd26e0d9768c4b0e7659686f5592
Thijs Triemstra <info@collab.nl>
2017-10-30 02:01:11 +0100

    bump luma.core version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     namespace_packages=["luma"],
     packages=["luma.lcd"],
     zip_safe=False,
-    install_requires=["luma.core>=1.0.1"],
+    install_requires=["luma.core>=1.1.0"],
     setup_requires=pytest_runner,
     tests_require=test_deps,
     extras_require={

aeaf627d9055f8f7303438a5034d8a6eb4157b08
Thijs Triemstra <info@collab.nl>
2017-11-01 22:58:35 +0100

    split baseline data into separate json files (#56)
    * ignore eclipse project files
    * split baseline data into separate json files
    * pip install on readthedocs
    
    * rename to get_reference_data

--- tests/test_ht1621.py
+++ tests/test_ht1621.py
@@ -3,8 +3,13 @@
 # Copyright (c) 2013-17 Richard Hull and contributors
 # See LICENSE.rst for details.
 
+"""
+Tests for the :py:class:`luma.lcd.device.ht1621` device.
+"""
+
 from luma.lcd.device import ht1621
 from luma.core.virtual import sevensegment
+
 from helpers import call, Mock  # noqa: F401
 
 gpio = Mock(unsafe=True)

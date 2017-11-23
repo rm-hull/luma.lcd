2041bde5c6761cfb9a17a35f90597b44e0747132
Thijs Triemstra <info@collab.nl>
2017-11-23 05:10:42 +0100

    move version nr to lib (#59)
    * move version nr to lib
    * update manifest

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import re
 import os
 import sys
 from io import open
@@ -8,14 +9,26 @@ from setuptools import setup
 
 
 def read_file(fname, encoding='utf-8'):
-    with open(os.path.join(os.path.dirname(__file__), fname), encoding=encoding) as r:
+    with open(fname, encoding=encoding) as r:
         return r.read()
 
 
-README = read_file("README.rst")
-CONTRIB = read_file("CONTRIBUTING.rst")
-CHANGES = read_file("CHANGES.rst")
-version = read_file("VERSION.txt").strip()
+def find_version(*file_paths):
+    fpath = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = read_file(fpath)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+
+    err_msg = 'Unable to find version string in {}'.format(fpath)
+    raise RuntimeError(err_msg)
+
+
+README = read_file('README.rst')
+CONTRIB = read_file('CONTRIBUTING.rst')
+CHANGES = read_file('CHANGES.rst')
+version = find_version('luma', 'lcd', '__init__.py')
 
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
@@ -30,10 +43,10 @@ setup(
     version=version,
     author="Richard Hull",
     author_email="richard.hull@destructuring-bind.org",
-    description=("A library to drive PCD8544 and ST7735 LCDs"),
+    description=("A library to drive PCD8544, HT1621, ST7735 and UC1701X-based LCDs"),
     long_description="\n\n".join([README, CONTRIB, CHANGES]),
     license="MIT",
-    keywords="raspberry pi rpi lcd nokia 5110 display screen pcd8544 st7735 spi 84x48 160x128",
+    keywords="raspberry pi rpi lcd nokia 5110 display screen pcd8544 st7735 uc1701x ht1621 spi 84x48 160x128",
     url="https://github.com/rm-hull/luma.lcd",
     download_url="https://github.com/rm-hull/luma.lcd/tarball/" + version,
     namespace_packages=["luma"],
